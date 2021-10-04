a92652e76d222677ecca8a24c8be6d0e1c9d17db
Karleon Cristophe <karleoncristophe@hotmail.com>
2021-10-04 12:02:07 -0300

    router me added

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,11 @@ app.get('/clients', authorization, async (req, res) => {
    res.send(name);
 });
 
+app.get('/me', authenticate, async (req, res) => {
+   const user = await User.findOne({ _id: req.logged });
+   res.status(200).send(user);
+});
+
 app.post('/createAccount', async (req, res) => {
    const { name, email, password } = req.body;
 
